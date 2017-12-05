9dddf337cab17ad7b9128cf76c6fe61d41dc40d1
Blake Engelking <engelkinbr@scottsvalleyusd.org>
2017-12-05 17:45:50 +0000

    first commit

---
+++ simple-todos/imports/api/tasks.js
@@ -0,0 +1,46 @@
+import { Mongo } from 'meteor/mongo';
+export const Tasks = new Mongo.Collection('tasks');
+import { Meteor } from 'meteor/meteor';
+import { check } from 'meteor/check';
+ 
+Meteor.methods({
+  'tasks.insert'(text) {
+    check(text, String);
+ 
+    // Make sure the user is logged in before inserting a task
+    if (! Meteor.userId()) {
+      throw new Meteor.Error('not-authorized');
+    }
+ 
+    Tasks.insert({
+      text,
+      createdAt: new Date(),
+      owner: Meteor.userId(),
+      username: Meteor.user().username,
+    });
+  },
+  'tasks.remove'(taskId) {
+    check(taskId, String);
+ 
+    Tasks.remove(taskId);
+  },
+  'tasks.setChecked'(taskId, setChecked) {
+    check(taskId, String);
+    check(setChecked, Boolean);
+ 
+    Tasks.update(taskId, { $set: { checked: setChecked } });
+  },
+});
+
+if (Meteor.isServer) {
+  // This code only runs on the server
+  // Only publish tasks that are public or belong to the current user
+  Meteor.publish('tasks', function tasksPublication() {
+    return Tasks.find({
+      $or: [
+        { private: { $ne: true } },
+        { owner: this.userId },
+      ],
+    });
+  });
+}
